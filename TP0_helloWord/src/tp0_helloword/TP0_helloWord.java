96ab9d36046dc3834e79d593faa749805ab8b9d2
Noa Chemama <noa.chemama@epfedu.fr>
2025-10-22 15:25:41 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_helloWord/src/tp0_helloword/TP0_helloWord.java
@@ -0,0 +1,31 @@
+/*
+ * TP0 HelloWord 20/10
+ */
+package tp0_helloword;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Noa Chemama
+ */
+public class TP0_helloWord {
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 20;
+        String prenom ;
+Scanner sc ;
+sc = new Scanner (System.in) ;
+System.out.println("Quel est votre pre?nom ?");
+prenom = sc.nextLine();
+        System.out.println( " Bonjour " ) ;
+        System.out.println("au revoir");
+        // TODO code application logic here
+    }
+    
+}
