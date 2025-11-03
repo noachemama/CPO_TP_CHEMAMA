9eb60e8ab382494c21f74e5e3463e6aa89afe8b5
Noa Chemama <noa.chemama@epfedu.fr>
2025-11-03 15:28:11 +0100

    TP2 Bieres

---
+++ TP2_Bieres_CHEMAMA_/src/tp2_bieres_chemama_/TP2_Bieres_CHEMAMA_.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_chemama_;
+
+/**
+ *
+ * @author Noa Chemama
+ */
+public class TP2_Bieres_CHEMAMA_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    BouteilleBiere uneBiere = new BouteilleBiere();
+    uneBiere.nom = "Cuvée des trolls";
+    uneBiere.degreAlcool = 7.0 ;
+    uneBiere.brasserie ="Dubuisson";
+    uneBiere.ouverte=false;
+    BouteilleBiere deuxBiere = new BouteilleBiere();
+    deuxBiere.nom = "Leffe";
+    deuxBiere.degreAlcool=6.6;
+    deuxBiere.brasserie="Abbaye de Leffe";
+    uneBiere.lireEtiquette();
+    }
+    
+}
