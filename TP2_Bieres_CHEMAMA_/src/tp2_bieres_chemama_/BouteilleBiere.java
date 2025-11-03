7ee236c44052205fee7e74a0f0883acf2ed1c12d
Noa Chemama <noa.chemama@epfedu.fr>
2025-11-03 15:27:12 +0100

    TP2 Bieres

---
+++ TP2_Bieres_CHEMAMA_/src/tp2_bieres_chemama_/BouteilleBiere.java
@@ -0,0 +1,24 @@
+package tp2_bieres_chemama_;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Noa Chemama
+ */
+public class BouteilleBiere(String nom,double degreAlcool,String brasserie) {
+    public String nom;
+    public double degreAlcool;
+    public String brasserie;
+    public boolean ouverte;
+    public void lireEtiquette() {
+ 
+              
+                
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+}
+    
+}
