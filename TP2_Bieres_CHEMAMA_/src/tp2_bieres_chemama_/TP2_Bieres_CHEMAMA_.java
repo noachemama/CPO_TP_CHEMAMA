41889ffe0a877e1457f2b5c287ad484da4ddaafa
Noa Chemama <noa.chemama@epfedu.fr>
2025-11-04 11:07:35 +0100

    TP2 Bieres

--- TP2_Bieres_CHEMAMA_/src/tp2_bieres_chemama_/TP2_Bieres_CHEMAMA_.java
+++ TP2_Bieres_CHEMAMA_/src/tp2_bieres_chemama_/TP2_Bieres_CHEMAMA_.java
@@ -14,16 +14,24 @@ public class TP2_Bieres_CHEMAMA_ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    BouteilleBiere uneBiere = new BouteilleBiere();
+    BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls",7.0,"Duboisson");
     uneBiere.nom = "Cuvée des trolls";
     uneBiere.degreAlcool = 7.0 ;
     uneBiere.brasserie ="Dubuisson";
     uneBiere.ouverte=false;
-    BouteilleBiere deuxBiere = new BouteilleBiere();
-    deuxBiere.nom = "Leffe";
-    deuxBiere.degreAlcool=6.6;
-    deuxBiere.brasserie="Abbaye de Leffe";
+    BouteilleBiere deuxBiere = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe");
+    BouteilleBiere troisBiere = new BouteilleBiere("Pietra",7.2,"Brasserie Pietra");
+    BouteilleBiere quatreBiere =new BouteilleBiere("jean",6.2,"Nemar");
+    BouteilleBiere cinqBiere =new BouteilleBiere("Aller l'OM",13,"A jamais les premiers");
+           
+    System.out.println(uneBiere) ;
+    
+    uneBiere.Decapsuler();
     uneBiere.lireEtiquette();
+    deuxBiere.lireEtiquette();
+    troisBiere.lireEtiquette();
+    quatreBiere.lireEtiquette();
+    cinqBiere.lireEtiquette();
     }
     
 }
