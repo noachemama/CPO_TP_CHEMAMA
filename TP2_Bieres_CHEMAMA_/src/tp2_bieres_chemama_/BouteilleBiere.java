b5af9743eebdbef49a179cac5c8e4aa5afd9f689
Noa Chemama <noa.chemama@epfedu.fr>
2025-11-04 11:15:32 +0100

    TP2 Bieres

--- TP2_Bieres_CHEMAMA_/src/tp2_bieres_chemama_/BouteilleBiere.java
+++ TP2_Bieres_CHEMAMA_/src/tp2_bieres_chemama_/BouteilleBiere.java
@@ -9,16 +9,36 @@ package tp2_bieres_chemama_;
  *
  * @author Noa Chemama
  */
-public class BouteilleBiere(String nom,double degreAlcool,String brasserie) {
+public class BouteilleBiere{
     public String nom;
     public double degreAlcool;
     public String brasserie;
     public boolean ouverte;
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+nom = unNom;
+degreAlcool = unDegre;
+brasserie = uneBrasserie;
+ouverte = false;
+} 
+    void Decapsuler(){
+        if (!ouverte){
+            ouverte=true ;
+        }
+        else{ 
+           System.out.println("erreur la biere et deja ouverte");
+           ouverte=false;
+    }}
+    @Override
+public String toString() {
+    String chaine_a_retourner;
+    chaine_a_retourner = nom + " (" + degreAlcool + " degrés)Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
     public void lireEtiquette() {
- 
-              
-                
-System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+                            
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie+"\nouverte:  "+ouverte ) ;
 }
     
 }
