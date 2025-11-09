75fa3f12e93963c46f32e19ff078b7a6e117bc79
Noa Chemama <noa.chemama@epfedu.fr>
2025-11-09 17:51:37 +0100

    TP2 Manip CHEMAMA

---
+++ TP2_manip_CHEMAMA/src/tp2_manip_chemama/Moussaka.java
@@ -0,0 +1,20 @@
+package tp2_manip_chemama;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Noa Chemama
+ */
+public class Moussaka {
+int nbCalories;  
+public Moussaka(int nbCalories) {
+        this.nbCalories = nbCalories;
+}
+
+}
+
+
---
+++ TP2_manip_CHEMAMA/src/tp2_manip_chemama/Tartiflette.java
@@ -0,0 +1,17 @@
+package tp2_manip_chemama;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Noa Chemama
+ */
+public class Tartiflette {
+ int nbCalories; 
+ 
+ public Tartiflette(int nbCalories) {
+        this.nbCalories = nbCalories;
+}}
