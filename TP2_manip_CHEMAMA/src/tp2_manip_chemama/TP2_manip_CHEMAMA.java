324d7af9f909fa54d5d530a45e89b06f89d34a8a
Noa Chemama <noa.chemama@epfedu.fr>
2025-11-09 17:51:06 +0100

    TP2 Manip CHEMAMA

---
+++ TP2_manip_CHEMAMA/src/tp2_manip_chemama/TP2_manip_CHEMAMA.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_chemama;
+
+/**
+ *
+ * @author Noa Chemama
+ */
+public class TP2_manip_CHEMAMA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+Tartiflette assiette1 = new Tartiflette(500) ;
+Tartiflette assiette2 = new Tartiflette(600) ; 
+Tartiflette assiette3 = assiette2 ;
+assiette2 = assiette1;
+assiette1 = assiette3;
+
+Moussaka[] tableau = new Moussaka[10];{
+ for (int i = 0; i < 10; i++) {
+            tableau[i] = new Moussaka(100+100*i); }
+    }
+
+System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories);
+System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
+    }}
+
+
