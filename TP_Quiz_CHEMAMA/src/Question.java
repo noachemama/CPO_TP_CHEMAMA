b64601f7a940053823cf8a31a8f637d524020714
Noa Chemama <noa.chemama@epfedu.fr>
2025-11-26 17:12:52 +0100

    TP Quiz CHEMAMA

---
+++ TP_Quiz_CHEMAMA/src/Question.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Noa Chemama
+ */
+public class Question {
+private String intitule;
+private String proposition1;
+private String proposition2;
+private String proposition3;
+private String proposition4;
+private int indexBonneReponse; 
+
+    public Question(String intitule, String proposition1, String proposition2, String proposition3, String proposition4, int indexBonneReponse) {
+        this.intitule = intitule;
+        this.proposition1 = proposition1;
+        this.proposition2 = proposition2;
+        this.proposition3 = proposition3;
+        this.proposition4 = proposition4;
+        this.indexBonneReponse = indexBonneReponse;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public String getProposition1() {
+        return proposition1;
+    }
+
+    public String getProposition2() {
+        return proposition2;
+    }
+
+    public String getProposition3() {
+        return proposition3;
+    }
+
+    public String getProposition4() {
+        return proposition4;
+    }
+
+    public int getIndexBonneReponse() {
+        return indexBonneReponse;
+    }
+
+
+}
