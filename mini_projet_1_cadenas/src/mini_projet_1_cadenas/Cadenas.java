7ef6ea2a2ecad5c698959157afe63796120c80d1
Noa Chemama <noa.chemama@epfedu.fr>
2025-12-08 17:19:42 +0100

    J'ai permis d'augmenter les compteurs et j'ai commencé a afficher nombres trop grands ,nombrse trop petits.......

--- mini_projet_1_cadenas/src/mini_projet_1_cadenas/Cadenas.java
+++ mini_projet_1_cadenas/src/mini_projet_1_cadenas/Cadenas.java
@@ -14,18 +14,25 @@ import java.util.ArrayList;
 public class Cadenas extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Cadenas.class.getName());
-
+    int[] chiffres = {0,1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+    int compteur1=0;
+    int compteur2=0;
+    int compteur3=0;
+    int compteur4=0;
+    int resultatUtilisateur=0;
+    int nombreTentative=0;
+    int number = (int)(Math.random() * 10000); 
+    String resultat = String.format("%04d", number);
+    int valeur = Integer.parseInt(resultat);
     public Cadenas() {
         initComponents();
-        int[] chiffres = {0,1, 2, 3, 4, 5, 6, 7, 8, 9};
         
         texte_fin.setVisible(false);
         texte_chiffre_0.setText(String.valueOf(chiffres[0]));
         texte_chiffre_1.setText(String.valueOf(chiffres[0]));
         texte_chiffre_2.setText(String.valueOf(chiffres[0]));
         texte_chiffre_3.setText(String.valueOf(chiffres[0]));
-        int number = (int)(Math.random() * 10000); 
-        String resultat = String.format("%04d", number);
+        int a=0;
     }
 
     /**
@@ -97,6 +104,11 @@ public class Cadenas extends javax.swing.JFrame {
 
                     up_chiffre_4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
                     up_chiffre_4.setText("/\\");
+                        up_chiffre_4.addActionListener(new java.awt.event.ActionListener() {
+                            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                up_chiffre_4ActionPerformed(evt);
+                            }
+                        });
                         getContentPane().add(up_chiffre_4, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 70, -1, -1));
 
                         texte_chiffre_0.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
@@ -117,21 +129,46 @@ public class Cadenas extends javax.swing.JFrame {
 
                         down_chiffre_1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
                         down_chiffre_1.setText("\\/");
+                        down_chiffre_1.addActionListener(new java.awt.event.ActionListener() {
+                            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                down_chiffre_1ActionPerformed(evt);
+                            }
+                        });
                         getContentPane().add(down_chiffre_1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, -1, -1));
 
                         down_chiffre_2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
                         down_chiffre_2.setText("\\/");
+                        down_chiffre_2.addActionListener(new java.awt.event.ActionListener() {
+                            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                down_chiffre_2ActionPerformed(evt);
+                            }
+                        });
                         getContentPane().add(down_chiffre_2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 160, -1, -1));
 
                         down_chiffre_3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
                         down_chiffre_3.setText("\\/");
+                        down_chiffre_3.addActionListener(new java.awt.event.ActionListener() {
+                            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                down_chiffre_3ActionPerformed(evt);
+                            }
+                        });
                         getContentPane().add(down_chiffre_3, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 160, -1, -1));
 
                         down_chiffre_4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
                         down_chiffre_4.setText("\\/");
+                        down_chiffre_4.addActionListener(new java.awt.event.ActionListener() {
+                            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                down_chiffre_4ActionPerformed(evt);
+                            }
+                        });
                         getContentPane().add(down_chiffre_4, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 160, -1, -1));
 
                         bouton_tester.setText("Tester");
+                        bouton_tester.addActionListener(new java.awt.event.ActionListener() {
+                            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                bouton_testerActionPerformed(evt);
+                            }
+                        });
                         getContentPane().add(bouton_tester, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 120, -1, -1));
 
                         texte_lbl_nb_chiffres_exacts.setText("Nombre de chiffres exacts :");
@@ -175,11 +212,15 @@ public class Cadenas extends javax.swing.JFrame {
                     }// </editor-fold>//GEN-END:initComponents
 
     private void up_chiffre_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_2ActionPerformed
-        // TODO add your handling code here:
+compteur2=compteur2+1;
+  
+        texte_chiffre_1.setText(String.valueOf(chiffres[compteur2]));     
     }//GEN-LAST:event_up_chiffre_2ActionPerformed
 
     private void up_chiffre_3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_3ActionPerformed
-        // TODO add your handling code here:
+compteur3=compteur3+1;
+  
+        texte_chiffre_2.setText(String.valueOf(chiffres[compteur3]));
     }//GEN-LAST:event_up_chiffre_3ActionPerformed
 
     private void bouton_recommencerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouton_recommencerActionPerformed
@@ -187,10 +228,51 @@ public class Cadenas extends javax.swing.JFrame {
     }//GEN-LAST:event_bouton_recommencerActionPerformed
 
     private void up_chiffre_1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_1ActionPerformed
-
+        compteur1=compteur1+1;
   
-        texte_chiffre_0.setText(String.valueOf(chiffres[0]));
+        texte_chiffre_0.setText(String.valueOf(chiffres[compteur1]));
     }//GEN-LAST:event_up_chiffre_1ActionPerformed
+
+    private void down_chiffre_1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_1ActionPerformed
+        compteur1=compteur1-1;
+  
+        texte_chiffre_0.setText(String.valueOf(chiffres[compteur1]));
+    }//GEN-LAST:event_down_chiffre_1ActionPerformed
+
+    private void down_chiffre_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_2ActionPerformed
+compteur2=compteur2-1;
+  
+        texte_chiffre_1.setText(String.valueOf(chiffres[compteur2]));
+    }//GEN-LAST:event_down_chiffre_2ActionPerformed
+
+    private void down_chiffre_3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_3ActionPerformed
+compteur3=compteur3-1;
+  
+        texte_chiffre_2.setText(String.valueOf(chiffres[compteur3]));
+    }//GEN-LAST:event_down_chiffre_3ActionPerformed
+
+    private void up_chiffre_4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_4ActionPerformed
+compteur4=compteur4+1;
+  
+        texte_chiffre_3.setText(String.valueOf(chiffres[compteur4]));
+    }//GEN-LAST:event_up_chiffre_4ActionPerformed
+
+    private void down_chiffre_4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_4ActionPerformed
+compteur4=compteur4-1;
+  
+        texte_chiffre_3.setText(String.valueOf(chiffres[compteur4]));
+    }//GEN-LAST:event_down_chiffre_4ActionPerformed
+
+    private void bouton_testerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouton_testerActionPerformed
+resultatUtilisateur=compteur1*1000+compteur2*100+compteur3*10+compteur4;
+if (resultatUtilisateur==valeur){
+    texte_fin.setText("VOUS AVEZ GAGNEEEEEEE !!!!!");
+    texte_fin.setVisible(true);
+}else {
+    nombreTentative++;
+}
+if (nombreTentative
+    }//GEN-LAST:event_bouton_testerActionPerformed
 
     /**
      * @param args the command line arguments
