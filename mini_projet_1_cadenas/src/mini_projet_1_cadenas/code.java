daeefd258d9f50beaf1a5082051b0146a251942f
Noa Chemama <noa.chemama@epfedu.fr>
2025-12-12 17:10:22 +0100

    Projet terminé !!!

--- mini_projet_1_cadenas/src/mini_projet_1_cadenas/code.java
+++ mini_projet_1_cadenas/src/mini_projet_1_cadenas/code.java
@@ -10,39 +10,16 @@ package mini_projet_1_cadenas;
  * @author Noa Chemama
  */
 public class code {
-    int nb1;
-    int nb2;
-    int nb3;
-    int nb4;
-    int resultat;
+    
 
-    public code(int nb1, int nb2, int nb3, int nb4, int resultat) {
-        this.nb1 = nb1;
-        this.nb2 = nb2;
-        this.nb3 = nb3;
-        this.nb4 = nb4;
-        this.resultat = resultat;
-    }
-
-    public int getNb1() {
-        return nb1;
-    }
-
-    public int getNb2() {
-        return nb2;
-    }
-
-    public int getNb3() {
-        return nb3;
-    }
-
-    public int getNb4() {
-        return nb4;
-    }
-
-    public int getResultat() {
-        return resultat;
-    }
+   public int code2 (int codeBon, int codeUtilisateur){
+       
+       if (codeBon==codeUtilisateur){
+           return 1;
+       }else{       
+       return 0;
+       }
+   }}
     
     
-}
+
